b5a1018c06c5af97f1223ebb4355808109e0b608
Fernando Batista <fernandohsbatista@gmail.com>
2025-06-18 22:24:58 -0300

    ms-catalog-admin - (refactor):use constants to name length on Category

--- ms-catalog-admin/domain/src/main/java/com/cineflix/admin/catalog/domain/validation/CategoryValidator.java
+++ ms-catalog-admin/domain/src/main/java/com/cineflix/admin/catalog/domain/validation/CategoryValidator.java
@@ -3,6 +3,8 @@ package com.cineflix.admin.catalog.domain.validation;
 import com.cineflix.admin.catalog.domain.category.Category;
 
 public class CategoryValidator extends Validator {
+    public static final int MIN_NAME_LENGTH = 3;
+    public static final int MAX_NAME_LENGTH = 255;
     private final Category category;
 
     public CategoryValidator(Category category, ValidationHandler handler) {
@@ -29,7 +31,7 @@ public class CategoryValidator extends Validator {
         }
 
         final var nameLength = name.length();
-        if (nameLength < 3 || nameLength > 255) {
+        if (nameLength < MIN_NAME_LENGTH || nameLength > MAX_NAME_LENGTH) {
             this.validationHandler().append(new Error("'name' must be between 3 and 255 chars"));
         }
     }
